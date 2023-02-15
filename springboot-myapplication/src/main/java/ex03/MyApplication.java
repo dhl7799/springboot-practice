4692f3f6b2fc96e74f6135abe5718f23c9803123
dhl7799 <donghyundhl@naver.com>
2023-02-15 15:58:41 +0900

    springboot

--- springboot-myapplication/src/main/java/ex03/MyApplication.java
+++ springboot-myapplication/src/main/java/ex03/MyApplication.java
@@ -3,9 +3,7 @@ package ex03;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootConfiguration
 @ComponentScan
@@ -13,5 +11,4 @@ public class MyApplication {
 	public static void main(String[] args) {
 		try(ConfigurableApplicationContext ac = SpringApplication.run(MyApplication.class, args)){}
 	}
-
 }
